54aca429d6c7116192c576f977e7f439f3b2e959
sourobrata.sarkar <sourobrata.sarkar@mountblue.io>
2019-04-03 17:47:49 +0530

    FEAT: Error handling and logging

--- src/server/routes/authRoute.js
+++ src/server/routes/authRoute.js
@@ -3,47 +3,56 @@ const Joi = require("joi");
 
 const userJoiSchema = require("../../joiSchemas/userSchema");
 const sequelize = require("sequelize");
+const logger = require("../winstonLogger");
 require("../../db/connectToDB");
 const User = require("../../models/user");
 const { encrypt, compare } = require("../../utilities/hashingFunctions");
 const generateToken = require("../../utilities/generateToken");
 
 //login end-point
-routes.post("/login", async (req, res) => {
-  let { email, password } = req.body;
-  let { error, value } = Joi.validate({ email, password }, userJoiSchema);
-  if (error) res.status(400).send("Error in request" + error);
+routes.post("/login", async (req, res, next) => {
+  try {
+    let { email, password } = req.body;
+    let { error, value } = Joi.validate({ email, password }, userJoiSchema);
+    if (error) throw { code: 400, message: error };
 
-  //check if user is in db
-  const userInDB = await User.findOne({ where: { email: value.email } });
-  if (!userInDB) {
-    res.status(400).send("User not found");
-  } else {
-    const storedPassword = userInDB.dataValues.password;
-    if (!(await compare(value.password, storedPassword))) res.status(400).send("Email/Password do not match");
-    else {
-      res.status(200).send(generateToken({ id: userInDB.dataValues.id, email: userInDB.dataValues.email }));
+    //check if user is in db
+    const userInDB = await User.findOne({ where: { email: value.email } });
+    if (!userInDB) {
+      throw { code: 404, message: "User not found" };
+    } else {
+      const storedPassword = userInDB.dataValues.password;
+      if (!(await compare(value.password, storedPassword))) throw { code: 400, message: "Email/Password do not match" };
+      else {
+        res.status(200).send(generateToken({ id: userInDB.dataValues.id, email: userInDB.dataValues.email }));
+      }
     }
+  } catch (e) {
+    next(e);
   }
 });
 
 //register end-point
-routes.post("/register", async (req, res) => {
-  let { email, password } = req.body;
-  let { error, value } = Joi.validate({ email, password }, userJoiSchema);
-  if (error) res.status(400).send("Error in request" + error);
-  //check if user exists, if not, store to db with hashed password
-  const userInDB = await User.findOne({ where: { email: value.email } });
-  if (userInDB) {
-    res.status(400).send("User is already present");
-  } else {
-    const hashedPassword = await encrypt(value.password);
-    const newUser = await User.create({
-      email: value.email,
-      password: hashedPassword
-    });
-    //generate token and return response
-    res.status(200).send(generateToken({ id: newUser.id, email: newUser.email }));
+routes.post("/register", async (req, res, next) => {
+  try {
+    let { email, password } = req.body;
+    let { error, value } = Joi.validate({ email, password }, userJoiSchema);
+    if (error) throw { code: 400, message: error };
+    //check if user exists, if not, store to db with hashed password
+    const userInDB = await User.findOne({ where: { email: value.email } });
+    if (userInDB) {
+      throw { code: 400, message: "User is already present" };
+    } else {
+      const hashedPassword = await encrypt(value.password);
+      const newUser = await User.create({
+        email: value.email,
+        password: hashedPassword
+      });
+      //generate token and return response
+      res.status(200).send(generateToken({ id: newUser.id, email: newUser.email }));
+    }
+  } catch (e) {
+    next(e);
   }
 });
 
--- src/server/server.js
+++ src/server/server.js
@@ -2,11 +2,24 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const authRoute = require("./routes/authRoute");
+const logger = require("../winstonLogger");
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use("/auth", authRoute);
+
+//error handling middleware
+app.use(function(err, req, res, next) {
+  logger.log({
+    level: "error",
+    message: err
+  });
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(err.code || 500).send("Error: " + err.message);
+});
 
 app.listen(3001, function() {
   console.log("listening on port 3001!");
